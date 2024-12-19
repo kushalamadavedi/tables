318b576f4de93b44c53a42fc49f7902e9cc63ded
madavedikushala <kushalamadavedi07@gmail.com>
2024-12-19 13:10:16 +0530

    table

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { tablecomponent } from 'src/shared/component/table/table/table.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    tablecomponent,
   ],
   imports: [
     BrowserModule
